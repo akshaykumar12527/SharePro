f74f028bd6740c99905197e12fb2e86867645b3c
Akshay kumar <akshaykumar12527@gmail.com>
2015-09-26 10:28:05 +0530

    second commit

--- tables/tables.js
+++ tables/tables.js
@@ -5,7 +5,7 @@ methods.insertUser = function(data,cb){
 };
 methods.getCardByUserID=function(userid,cb){
 var found=false;
-	var database=JSON.parse((fs.readFileSync("./database/SharePro.json")).toString());
+	var database=JSON.parse((fs.readFileSync("../database/SharePro.json")).toString());
 	 for(var i=0;i<database.card.length;i++){
 		 if(database.card[i].userid==userid)
 			{
