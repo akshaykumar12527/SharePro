86dd3074464620f46d24804326092bd733a047c6
Akshay kumar <akshaykumar12527@gmail.com>
2015-09-27 02:51:05 +0530

    second commit

--- index.js
+++ index.js
@@ -72,10 +72,21 @@ router.route('/authenticate/:phoneNumber/:OTP')
 	if(User){
 		console.log(User,res.body);
 		if(req.params.OTP==User.OTP && req.params.phoneNumber==User.phoneNumber){
-			response.error=false;
-			response.data=req.params.phoneNumber;
-			response.userMessage='Authenticated successfully';
-			SendResponse(res);
+			tables.insertUser(User,function(done){
+				if(done){
+					response.error=false;
+					response.data=req.params.phoneNumber;
+					response.userMessage='Authenticated successfully';
+					SendResponse(res);		
+				}	
+				else{
+					response.error=true;
+					response.data=null;
+					response.userMessage='Something went wrong';
+					SendResponse(res);			
+				}
+			});
+			
 		}
 		else{
 			response.error=true;
@@ -92,6 +103,35 @@ router.route('/authenticate/:phoneNumber/:OTP')
 	}
 });
 /****************************End of Login*****************************/
+/*****************************insert card route******************************/
+router.route('/insertCard/:phoneNumber/:name/:email/:position/:company/:facebook/:linkedin/:twitter')
+.post(function(req,res){
+	// User=req.session.User;
+	if(req.params.phoneNumber && req.params.phoneNumber.length==10){
+		// console.log(User,res.body);
+		tables.insertCard(req.params,function(done){
+			if(data,done){
+				response.error=false;
+				response.data=data;
+				response.userMessage='Card Created successfully';
+				SendResponse(res);		
+			}	
+			else{
+				response.error=true;
+				response.data=null;
+				response.userMessage='Something went wrong';
+				SendResponse(res);			
+			}
+		});
+	}
+	else{
+		response.error=true;
+		response.data=null;
+		response.userMessage='Something went wrong';
+		SendResponse(res);
+	}
+});
+/****************************End of insert card*****************************/
 /*****************************Get cards by userid******************************/
 router.route('/getCards/:phoneNumber')
 .post(function(req,res){
@@ -122,7 +162,36 @@ router.route('/getCards/:phoneNumber')
 	});
 	
 /********************End of getcards by user***********************************************/
-
+/*****************************Share Cards route******************************/
+router.route('/shareCards/:user1/:user2')
+.post(function(req,res){
+	console.log('into shareCards');
+	if(req.params.user1 && req.params.user2 && req.params.user1.length==10 && req.params.user2.length==10){
+		tables.shareCards(req.params,function(data,done){
+			if(done){
+				response.error=false;
+			    response.data=data;
+			    response.userMessage='Cards shared successfully';
+			    SendResponse(res);
+			}
+			else{
+				response.error=true;
+		    	response.data=null;
+		    	response.userMessage='Something went wrong';
+		    	SendResponse(res);
+				
+			}
+		});
+	}
+	else{
+		response.error=true;
+		response.data=null;
+		response.userMessage='Something went wrong';
+		SendResponse(res);	
+	}
+	});
+	
+/********************End of Share Cards***********************************************/
 
 /*****************************Generate OTP for user******************************/
 router.route('/getOTP/:phoneNumber')
--- tables/tables.js
+++ tables/tables.js
@@ -1,13 +1,47 @@
 var fs = require("fs");
 var methods = new Object();
 methods.insertUser = function(data,cb){
+	try{
+		var database=JSON.parse((fs.readFileSync("./database/SharePro.json")).toString());
+		database.users.push(data);
+		fs.writeFileSync("./database/SharePro.json",JSON.stringify(database));
+		cb(true);
+	}
+	catch(Exception){
+		cb(false);
+	}
+
+};
+methods.insertCard = function(data,cb){
+	try{
+		var database=JSON.parse((fs.readFileSync("./database/SharePro.json")).toString());
+		data.card_id=database.card.length+1;
+		database.card.push(data);
+		fs.writeFileSync("./database/SharePro.json",JSON.stringify(database));
+		cb(data,true);
+	}
+	catch(Exception){
+		cb(data,false);
+	}
+
+};
+methods.shareCards = function(data,cb){
+	try{
+		var database=JSON.parse((fs.readFileSync("./database/SharePro.json")).toString());
+		database.friendship.push(data);
+		fs.writeFileSync("./database/SharePro.json",JSON.stringify(database));
+		cb(data,true);
+	}
+	catch(Exception){
+		cb(data,false);
+	}
 
 };
 methods.getCardByUserID=function(userid,cb){
-var found=false;
+	var found=false;
 	var database=JSON.parse((fs.readFileSync("./database/SharePro.json")).toString());
 	 for(var i=0;i<database.card.length;i++){
-		 if(database.card[i].userid==userid)
+		 if(database.card[i].phoneNumber==userid)
 			{
 				found = true;
 				data = database.card[i];
